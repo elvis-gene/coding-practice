edb32052f72af78e04fc6d50c63cb357e7b95dc8
Elvis Gene <elvis.pgene@gmail.com>
2020-09-04 09:46:07 +0200

    Ignore .idea folder

--- Udacity - interview prepa course practise/fibonnaci_num.py
+++ Udacity - interview prepa course practise/fibonnaci_num.py
@@ -1,17 +1,18 @@
 def fib_num(input_num):
 
-    if input_num == 1: return 0
-    if input_num == 2: return 1
-    if input_num < 1: return input_num # fibonacci of a negative number
-
+    if input_num == 1:
+        return 0
+    if input_num == 2:
+        return 1
+    if input_num < 1:
+        return input_num
 
     num, a, b = 0, 0, 1
     for i in range(input_num - 2):
         num = a + b
         a = b
         b = num
+        print(num)
 
-    return num
 
 print(fib_num(10))
-print(fib_num(-5))
