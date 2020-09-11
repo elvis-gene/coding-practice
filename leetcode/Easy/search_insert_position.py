a64661d7b68382471bb47564b120efb154f1d918
Elvis Gene <elvis.pgene@gmail.com>
2020-09-12 00:19:23 +0200

    Update search_insert_position.py

--- leetcode/Easy/search_insert_position.py
+++ leetcode/Easy/search_insert_position.py
@@ -1,4 +1,4 @@
-35. Search Insert Position
+""" 35. Search Insert Position
 
 Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 You may assume no duplicates in the array.
@@ -19,7 +19,7 @@ Example 4:
 
 Input: [1,3,5,6], 0
 Output: 0
-
+"""
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
