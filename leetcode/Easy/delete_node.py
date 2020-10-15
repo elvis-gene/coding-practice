387f206679d7893ad0754449f5f7c1d2d84188bc
Elvis Gene <elvis.pgene@gmail.com>
2020-10-15 17:16:37 +0200

    Create delete_node.py

---
+++ leetcode/Easy/delete_node.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+"""
+Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+
+It is guaranteed that the node to be deleted is not a tail node in the list.
+
+
+The number of the nodes in the given list is in the range [2, 1000].
+-1000 <= Node.val <= 1000
+The value of each node in the list is unique.
+The node to be deleted is in the list and is not a tail node
+"""
+
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        # Algorithm:
+        # - Replace node's value with the value of the next node
+        # Delete the next node
+        
+        node.val = node.next.val
+        node.next = node.next.next
