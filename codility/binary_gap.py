f68624e7a484a511c2659006c272e9c57cf29f43
Elvis Gene <elvis.pgene@gmail.com>
2020-11-27 20:12:21 +0200

    Create binary_gap.py

---
+++ codility/binary_gap.py
@@ -0,0 +1,45 @@
+"""
+BinaryGap
+Find longest sequence of zeros in binary representation of an integer.
+
+Task description
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+
+Write a function:
+
+def solution(N)
+
+that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..2,147,483,647].
+"""
+
+
+def solution(N):
+    # write your code in Python 3.6
+    bin_str = bin(N)[2:]
+
+    max_len = 0
+    char_frequency = dict()
+
+    for window_end in bin_str:
+
+        if window_end == '0':
+            if window_end not in char_frequency:
+                char_frequency[window_end] = 1
+            else:
+                char_frequency[window_end] += 1
+        
+        else:
+            if char_frequency:
+                max_len = max(max_len, char_frequency['0']) 
+                del char_frequency['0']
+                # char_frequency.pop('0')
+
+    return max_len
