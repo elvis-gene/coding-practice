7ff98c88b5b7fc6ec9c7aca952e1bef1f0446731
Elvis Gene <elvis.pgene@gmail.com>
2021-04-02 15:04:51 +0200

    Create 1805_Number_of_Different_Integers_in_a_String.py

---
+++ leetcode/Easy/1805_Number_of_Different_Integers_in_a_String.py
@@ -0,0 +1,56 @@
+"""
+1805. Number of Different Integers in a String
+
+You are given a string word that consists of digits and lowercase English letters.
+
+You will replace every non-digit character with a space. For example, "a123bc34d8ef34" will become " 123  34 8  34". Notice that you are left with some integers that are separated by at least one space: "123", "34", "8", and "34".
+
+Return the number of different integers after performing the replacement operations on word.
+
+Two integers are considered different if their decimal representations without any leading zeros are different.
+
+ 
+
+Example 1:
+
+Input: word = "a123bc34d8ef34"
+Output: 3
+Explanation: The three different integers are "123", "34", and "8". Notice that "34" is only counted once.
+Example 2:
+
+Input: word = "leet1234code234"
+Output: 2
+Example 3:
+
+Input: word = "a1b01c001"
+Output: 1
+Explanation: The three integers "1", "01", and "001" all represent the same integer because
+the leading zeros are ignored when comparing their decimal values.
+ 
+
+Constraints:
+
+1 <= word.length <= 1000
+word consists of digits and lowercase English letters.
+"""
+
+
+class Solution:
+    def numDifferentIntegers(self, word: str) -> int:
+        
+        # Algorithm
+        # Go over each character of the string
+        # if it is a letter, replace it with space
+        # Split the remaining string to a set of numbers
+        # return set length
+        
+        
+        for c in word:
+            if not c.isdigit():
+                word = word.replace(c, ' ')
+        
+        numbers = set(word.split())
+        numbers = set(int(i) for i in numbers)
+        
+        return len(numbers)
+        
